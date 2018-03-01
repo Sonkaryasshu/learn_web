8b1cade412af658b7c53b896ca087038b514ee41
Sonkaryasshu <you@example.com>
2018-03-01 20:34:38 +0530

    search

--- AJAX-JSON/js.js
+++ AJAX-JSON/js.js
@@ -17,6 +17,7 @@ btn.addEventListener('click', 	function(){
 	i++;
 	if(i>8){
 		document.getElementById('btn').style.display='none';
+		document.getElementById('myInput').style.display='block';
 	}
 })
 
@@ -34,4 +35,26 @@ function renderHTML(data){
 	}
 	string+="</td></tr>";
 	container.insertAdjacentHTML('beforeend', string);
+}
+
+
+
+
+function myFunction() {
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("table");
+  tr = table.getElementsByTagName("tr");
+
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[1];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
 }
